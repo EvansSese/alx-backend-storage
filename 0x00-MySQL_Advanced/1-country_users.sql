097bad3a5223716d00c34236887c2ecec6e6c37b
Evans Sese <sese_evans@yahoo.com>
2024-01-17 17:06:18 +0300

    [Feature] Task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- This script creates a table 'users' with specified attributes
+
+-- If the table already exists, it won't fail
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country VARCHAR(2) NOT NULL DEFAULT 'US',
+    CHECK (country IN ('US', 'CO', 'TN'))
+);
