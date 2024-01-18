9733b2af32c4e9dc7228307216f2d21166029335
Evans Sese <sese_evans@yahoo.com>
2024-01-18 14:56:47 +0300

    [Feature] Task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,17 +1,5 @@
 -- Calculate the rank of country origins based on the number of fans
-WITH CountryFansRank AS (
-    SELECT
-        origin,
-        COUNT(*) AS nb_fans
-    FROM
-        bands
-    GROUP BY
-        origin
-)
-SELECT
-    origin,
-    nb_fans
-FROM
-    CountryFansRank
-ORDER BY
-    nb_fans DESC;
+SELECT origin AS origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
