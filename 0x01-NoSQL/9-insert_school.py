56014d9ed53bfd96ff5c49b84dc74a09d4324709
Evans Sese <sese_evans@yahoo.com>
2024-01-22 17:44:05 +0300

    [Feature] Task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""MongoDB using python"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert new document into a collection based on kwargs"""
+    return mongo_collection.insert(kwargs)
