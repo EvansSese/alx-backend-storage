84b19b2d7633a1dd9725ee61242db776e5e16e74
Evans Sese <sese_evans@yahoo.com>
2024-01-22 05:54:02 -0800

    [Feature] Task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,8 +4,7 @@
 
 def list_all(mongo_collection):
     """List all docs in a collection"""
-    docs = mongo_collection.find()
+    docs = list(mongo_collection.find())
 
-    if docs.count() == 0:
-        return []
     return docs
+
