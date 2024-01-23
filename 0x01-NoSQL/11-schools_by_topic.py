84061c25e00c5aedcea577bce89c87ae0cda75b5
Evans Sese <sese_evans@yahoo.com>
2024-01-23 17:53:32 +0300

    [Feature] Task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,5 +3,6 @@
 
 
 def schools_by_topic(mongo_collection, topic):
+    """Find schools that teach certain topics"""
     school_list_with_topic = list(mongo_collection.find({"topics": topic}))
     return school_list_with_topic
