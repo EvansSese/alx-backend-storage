0e8e96a6452d506c036dabf05f60843755d9fc48
Evans Sese <sese_evans@yahoo.com>
2024-01-24 17:03:59 +0300

    [Feature] Task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -40,6 +40,19 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable) -> None:
+    """Replays the history of a function"""
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls = cache.get(name).decode("utf-8")
+    print("{} was called {} times:".format(name, calls))
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+                                     o.decode('utf-8')))
+
+
 class Cache:
     """Cache class for Redis"""
 
@@ -57,7 +70,7 @@ class Cache:
         return key
 
     def get(self, key: str, fn: Optional[Callable] = None) -> Union[
-            str, bytes, int, float, None]:
+        str, bytes, int, float, None]:
         """get function"""
         data = self._redis.get(key)
         if data is not None:
